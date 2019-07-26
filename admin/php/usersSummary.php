565458d84fddc40b8992e021069a8cfd4bc1a11d
collinsnyamao <Brightmeadow1>
2019-07-26 23:50:21 +0300

    added admin UI

---
+++ admin/php/usersSummary.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root3";
+$password = "123";
+$dbname = "evento";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "select * from Users;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+        echo "<tr>
+                      <td>".$row['user_id']."</td>
+                      <td>".$row['user_uid']."</td>
+                      <td>".$row['user_Birthday']."</td>
+                      <td>".$row['user_Email']."</td>
+                      <td>".$row['user_Full_Names']."</td>
+                      <td>".$row['user_Gender']."</td>
+                    </tr>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
