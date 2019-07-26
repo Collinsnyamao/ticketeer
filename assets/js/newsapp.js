ec3101766dbb60825dab93c7eade0249cd8ccecc
collinsnyamao <Brightmeadow1>
2019-07-26 18:09:33 +0300

    ui edits 16

--- assets/js/newsapp.js
+++ assets/js/newsapp.js
@@ -2,6 +2,19 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
         console.log(user);
+
+
+
+        $('#sendAmount').on('click',function () {
+           var payPhone = $('#phoneInput').val();
+           var payAmount = getQueryVariable("eventPrice");
+
+           console.log(payPhone,' ',payAmount);
+
+        });
+
+
+
     } else {
 
         console.log('user not signed in!');
