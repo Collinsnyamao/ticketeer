aaa14019c10a661baf245a5d8420b541120a81fb
collinsnyamao <Brightmeadow1>
2019-07-26 18:40:59 +0300

    added mpesa payment

--- assets/js/newsapp.js
+++ assets/js/newsapp.js
@@ -7,9 +7,24 @@ firebase.auth().onAuthStateChanged(function(user) {
 
         $('#sendAmount').on('click',function () {
            var payPhone = $('#phoneInput').val();
+           var quantity = $('#quantityInput').val();
            var payAmount = getQueryVariable("eventPrice");
+           var totalAmount = quantity * payAmount;
 
            console.log(payPhone,' ',payAmount);
+
+
+            $.ajax({
+                type: "post",
+                method: "POST",
+                data: {phoneNumber:payPhone,totalAmount:totalAmount},
+                url: "php/payMpesa2.php",
+                success: function (response) {
+                    console.log('response3: '+ response);
+                }
+            });
+
+
 
         });
 
