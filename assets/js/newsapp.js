c5d9bff882b292a869a14e9abbfb3b6ca7a03387
collinsnyamao <Brightmeadow1>
2019-07-28 00:29:56 +0300

    mpesa callback final tests final 9

--- assets/js/newsapp.js
+++ assets/js/newsapp.js
@@ -43,3 +43,9 @@ firebase.auth().onAuthStateChanged(function(user) {
         // No user is signed in.
     }
 });
+
+
+var source = new EventSource("php/mPesa/callback.php");
+source.onmessage = function(event) {
+    console.log(event.data);
+};
