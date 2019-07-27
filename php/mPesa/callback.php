9f134fd0cf0773da908dadd3ee69c2f6dfdf630a
collinsnyamao <Brightmeadow1>
2019-07-27 18:41:45 +0300

    mpesa callback tests 3

--- php/mPesa/callback.php
+++ php/mPesa/callback.php
@@ -1,61 +1,50 @@
 <?php
-//Process Callback after transaction
-$dataPOST = trim(file_get_contents('php://input'));
-//Parse the xml data
-$xml = simplexml_load_string($dataPOST);
-$ns = $xml->getNamespaces(true);
-$soap = $xml->children($ns['SOAP-ENV']);
-$sbody = $soap->Body;
-$mpesa_response = $sbody->children($ns['ns1']);
-$rstatus = $mpesa_response->processCheckOutResponse;
-$status = $rstatus->children();
-$s_msisdn = $status->MSISDN;
-$s_date = $status->{'M-PESA_TRX_DATE'};
-$s_transactionid = $status->{'M-PESA_TRX_ID'};
-$s_status = $status->TRX_STATUS;
-$s_returncode = $status->RETURN_CODE;
-$s_description = $status->DESCRIPTION;
-$s_merchant_transaction_id = $status->MERCHANT_TRANSACTION_ID;
-$s_encparams = $status->ENC_PARAMS;
-$s_txID = $status->TRX_ID;
+$callbackJSONData=file_get_contents('php://input');
+$callbackData=json_decode($callbackJSONData);
+$transactionType=$callbackData->TransactionType;
+$transID=$callbackData->TransID;
+$transTime=$callbackData->TransTime;
+$transAmount=$callbackData->TransAmount;
+$businessShortCode=$callbackData->BusinessShortCode;
+$billRefNumber=$callbackData->BillRefNumber;
+$invoiceNumber=$callbackData->InvoiceNumber;
+$orgAccountBalance=$callbackData->OrgAccountBalance;
+$thirdPartyTransID=$callbackData->ThirdPartyTransID;
+$MSISDN=$callbackData->MSISDN;
+$firstName=$callbackData->FirstName;
+$middleName=$callbackData->MiddleName;
+$lastName=$callbackData->LastName;
 //Save the returned data into the database or use it to finish certain operation.
 
-if($s_status=="Success"){
-    //Perfomr X operation
 
-    echo "this is ssid $s_msisdn";
+//Perfomr X operation
 
-
-    $servername = "localhost";
-    $username = "root3";
-    $password = "123";
-    $dbname = "evento";
+echo "this is ssid ".$lastName;
 
 
-    $conn = mysqli_connect($host,$username,$password,$db);
-
-    if ($conn->connect_error){
-        echo 'failed';
-    }else{
-        echo 'success';
-
-        $sql = "INSERT INTO Transactions(s_msisdn,s_date,s_transaction_id,s_status,s_return_code,s_description,s_merchant_transaction_id) VALUES ('$s_msisdn','$s_date','$s_transactionid','$s_status','$s_returncode','$s_description','$s_merchant_transaction_id');";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $conn->close();
+$servername = "localhost";
+$username = "root3";
+$password = "123";
+$dbname = "evento";
 
 
+$conn = mysqli_connect($host,$username,$password,$db);
 
+if ($conn->connect_error){
+    echo 'failed';
+}else{
+    echo 'success';
+
+    $sql = "INSERT INTO Transactions(s_msisdn,s_date,s_transaction_id,s_status,s_return_code,s_description,s_merchant_transaction_id) VALUES ('$MSISDN','$transAmount','$lastName','$firstName','$middleName','$businessShortCode','$transactionType');";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
+    $conn->close();
 
 
-}else{
-    //Perform X operation
+
 }
-?>
