79815d5047585441fb0750865799fc307c73892d
collinsnyamao <Brightmeadow1>
2019-07-27 23:36:20 +0300

    mpesa callback final tests final 7

--- php/mPesa/callback.php
+++ php/mPesa/callback.php
@@ -47,10 +47,12 @@ if ($conn->connect_error){
 }else{
     echo 'success';
 
+    $amnt = (string) $amount;
+
     $sql = "insert into Transactions(transactionMpesaID, transaction_resultCode, transaction_resultDesc,
                          transaction_merchantRequestID, transaction_checkoutRequestID, transaction_amount,
                          transaction_Balance, transactionDate, transaction_phoneNumber)
-                          VALUES ('$mpesaReceiptNumber','$resultCode','$resultDesc','$merchantRequestID','$checkoutRequestID','$amount','$b2CUtilityAccountAvailableFunds','$transactionDate','$phoneNumber');";
+                          VALUES ('$mpesaReceiptNumber','$resultCode','$resultDesc','$merchantRequestID','$checkoutRequestID','$amnt','$balance','$transactionDate','$phoneNumber');";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
