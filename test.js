0f4b7d419733ce767d9b9b191c5561fec937ff92
collinsnyamao <Brightmeadow1>
2019-07-28 00:42:19 +0300

    mpesa callback final tests final 12

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var source = new EventSource("php/mPesa/callback.php");
-source.onmessage = function(event) {
-    console.log(event.data);
-};
+if(typeof(EventSource) !== "undefined") {
+    var source = new EventSource("php/mPesa/callback.php");
+    source.onmessage = function(event) {
+        console.log(event.data);
+    };
+} else {
+    console.log("Sorry, your browser does not support server-sent events...");
+}
